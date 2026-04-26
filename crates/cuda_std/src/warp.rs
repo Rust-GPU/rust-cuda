145f93623005b2a33491b5f256b86bdcf1c417dd
39ali <alimilhim5@gmail.com>
2026-04-27 00:09:28 +0300

    fix warp assert on pow of 2

--- crates/cuda_std/src/warp.rs
+++ crates/cuda_std/src/warp.rs
@@ -766,7 +766,7 @@ unsafe fn warp_shuffle_32(
     }
 
     assert!(
-        !(width & (width - 1)) != 0 && width <= 32,
+        width != 0 && (width & (width - 1)) == 0 && width <= 32,
         "width must be a power of 2 and less than or equal to 32"
     );
 
